c55d1ee2dc778ab11289aaa3d413b098ec6dffd5
Dante <dantecoderhub@gmail.com>
2026-05-28 21:32:42 +0200

    feat(app): add main styles injection

--- app/runtime/appInit.js
+++ app/runtime/appInit.js
@@ -1,4 +1,23 @@
-console.log("Starting app")
+const addStyles = (styles) => {
+    styles.forEach(item => {
+        const newLink = document.createElement("link")
+        newLink.href = item
+        newLink.rel = "stylesheet"
+        document.head.appendChild(newLink)
+    })
+}
 
-const level = await import("/framework/runtime/level.js")
-level.addGlobal()
+const init = async () => {
+    console.log("Starting app")
+
+    const levelModule = await import("/framework/runtime/level.js")
+    await levelModule.addGlobal()
+    console.log(level)
+
+    const styles = [
+        `${level.route}/app/styles/main.css`
+    ]
+    addStyles(styles)
+
+}
+init()
